ee64117087419e20f74276da2dd180a2746c0b9e
Ali Fawzi <57192852+HeavyPuncher@users.noreply.github.com>
2020-07-25 01:01:27 +0100

    Add files via upload

---
+++ Individual Project/project.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+
+df = pd.read_csv('Video_Games_Sales_as_at_22_Dec_2016.csv')
+
+df.describe()
+
+df.head()
